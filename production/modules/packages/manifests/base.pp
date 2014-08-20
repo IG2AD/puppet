88c88077ba1931c411546f8c977a073bcbcc86ac
Daniel Shirley <daniel.l.shirley@hp.com>
2014-08-20 20:27:58 +0000

    added doc string to base packages #15
    Change-Id: I51ca343ef3de6866205995ca67d77022b56d1654

--- production/modules/packages/manifests/base.pp
+++ production/modules/packages/manifests/base.pp
@@ -1,3 +1,26 @@
+# == Define: Base Packages
+#
+# Packages to be installed on all servers.
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+# To include these packages please add base role:
+#
+# class { '::site::roles::base': }
+#
+# === Authors
+#
+# aditaa <aditaa@ig2ad.com>
+#
+# === Copyright
+#
+# GPL ver. 2.
+#
+
 class packages::base {
   $packages = ['git', 'vim-enhanced', 'ntp']
   package { $packages: ensure => latest }
