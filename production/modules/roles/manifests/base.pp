33484eeb9b8ac03162c6a28615618a81ba946cb9
Daniel Shirley <daniel.l.shirley@hp.com>
2014-08-20 21:21:49 +0000

    added doc string to base role #15
    Change-Id: I53360adc36e67ed63b3e2271dc15aaa6e0cbc86c

--- production/modules/roles/manifests/base.pp
+++ production/modules/roles/manifests/base.pp
@@ -1,4 +1,26 @@
-class roles::base {
+ #== Define: Base Role
+ #
+ # Role to be used on all servers.
+ #
+ # === Parameters
+ #
+ # None
+ #
+ # === Examples
+ #
+ # To include these packages please add base role:
+ #
+ # class { '::roles::base': }
+ #
+ # === Authors
+ #
+ # aditaa <aditaa@ig2ad.com>
+ #
+ # === Copyright
+ #
+ # GPL ver. 2.
+ #
+ class roles::base {
   anchor { '::roles::base': }
 
   Class {
