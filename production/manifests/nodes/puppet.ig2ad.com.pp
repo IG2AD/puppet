0de498fe247c32894bfa1062d271c7416e57fba8
Daniel Shirley <daniel.l.shirley@hp.com>
2014-08-20 19:23:19 +0000

    puppet-lint updates #15
    Change-Id: I7cd300687b3f02e1ff809e6f43ebc455c10e09ec

--- production/manifests/nodes/puppet.ig2ad.com.pp
+++ production/manifests/nodes/puppet.ig2ad.com.pp
@@ -2,18 +2,19 @@ node 'puppet.ig2ad.com' {
 class { '::roles::base': }
 
 file { 'puppet_repo_pull':
-  require => File[ [ "/scripts" ] ],
-  path => "/scripts/git_pull.sh",
-  ensure => present,
-  mode => 777,
-  source => "puppet:///scripts/git_pull.sh"
+  ensure  => present,
+  require => File[ [ '/scripts' ] ],
+  path    => '/scripts/git_pull.sh',
+  mode    => '0777',
+  source  => 'puppet:///scripts/git_pull.sh'
   }
 
 cron { 'pull_puppet_repo':
-  require => File[ [ "/scripts/git_pull.sh" ] ],
-  command => "/scripts/git_pull.sh https://github.com/IG2AD/puppet.git /etc/puppet/environments > /dev/null",
-  minute => '*/5',
-  ensure => present
+  ensure  => present,
+  require => File[ [ '/scripts/git_pull.sh' ] ],
+  command => '/scripts/git_pull.sh https://github.com/IG2AD/puppet.git
+             /etc/puppet/environments > /dev/null',
+  minute  => '*/5',
   }
 
 }
