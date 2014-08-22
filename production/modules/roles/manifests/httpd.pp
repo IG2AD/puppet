65604f344936bcbac2aad96a81c2df8f49c84ac9
Daniel Shirley <daniel.l.shirley@hp.com>
2014-08-22 16:14:41 +0000

    code cleanup #15
    Change-Id: Ifb3538318b98909b36668a6711b9a8db9a3e95ac

--- production/modules/roles/manifests/httpd.pp
+++ production/modules/roles/manifests/httpd.pp
@@ -1,3 +1,25 @@
+#== Define: HTTPD Role
+#
+# Role to be used on web servers.
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+# To include these packages please add httpd role:
+#
+# class { '::roles::httpd': }
+#
+# === Authors
+#
+# aditaa <aditaa@ig2ad.com>
+#
+# === Copyright
+#
+# GPL ver. 2.
+#
 class roles::httpd {
   anchor { '::roles::httpd': }
 
