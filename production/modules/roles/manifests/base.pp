14a8bcad038f47484947400ea650b4cbf02c0f58
Daniel Shirley <daniel.l.shirley@hp.com>
2014-08-22 16:10:11 +0000

    code cleanup #15
    Change-Id: I80014545903604947f57efccfa1521f1be356442

--- production/modules/roles/manifests/base.pp
+++ production/modules/roles/manifests/base.pp
@@ -1,34 +1,34 @@
- #== Define: Base Role
- #
- # Role to be used on all servers.
- #
- # === Parameters
- #
- # None
- #
- # === Examples
- #
- # To include these packages please add base role:
- #
- # class { '::roles::base': }
- #
- # === Authors
- #
- # aditaa <aditaa@ig2ad.com>
- #
- # === Copyright
- #
- # GPL ver. 2.
- #
- class roles::base {
+#== Define: Base Role
+#
+# Role to be used on all servers.
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+# To include these packages please add base role:
+#
+# class { '::roles::base': }
+#
+# === Authors
+#
+# aditaa <aditaa@ig2ad.com>
+#
+# === Copyright
+#
+# GPL ver. 2.
+#
+class roles::base {
   anchor { '::roles::base': }
 
   Class {
     require => Anchor['::roles::base'],
   }
 
-  file { "/scripts":
-    ensure => "directory",
+  file { '/scripts':
+    ensure => 'directory',
   }
 
   class { '::packages::base': }
